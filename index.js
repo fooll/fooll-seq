e27b72fcc5d9354cdb2d8c9875b0eae3b328e831
burawi <burawi.ba@gmail.com>
2017-03-29 20:44:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const Foola = require('foola');
+const Sequelize = require('sequelize');
+
+class FoollSeq extends Foola {
+
+  constructor(dirname, server) {
+    super(dirname, server);
+    this.loadModels();
+  }
+
+  loadDBConfig() {
+    var rcPath = path.resolve('.sequelizerc');
+    var rc = require(rcPath);
+    var configText = fs.readFileSync(rc.config);
+    var config = JSON.parse(configText);
+    return config[this.server.env];
+  }
+
+  loadModels() {
+    var db = this.loadDBConfig();
+    var sequelize = new Sequelize(db.database, db.username, db.password, db);
+    var models = {};
+    var modelsFolderPath = path.resolve(this.dirname, 'models');
+    var modelsDirFiles = fs.readdirSync(modelsFolderPath);
+    modelsDirFiles.forEach(function (modelBase) {
+      var modelPath = path.resolve(modelsFolderPath, modelBase);
+      var model = sequelize.import(modelPath);
+      var modelName = path.parse(modelBase).name;
+      models[modelName] = model;
+    });
+    this.models = models;
+  }
+
+  static coordinateDB(server) {
+    var db = {};
+
+    for (var moduleName in server.modules) {
+      var models = server.modules[moduleName];
+      Object.assign(db, module.models);
+    }
+
+    for (var moduleName in server.modules) {
+      for (var modelName in server.modules[moduleName].models) {
+        if (server.modules[moduleName].models[modelName].associate) {
+          server.modules[moduleName].models[modelName].associate(db);
+        }
+      }
+    }
+
+  }
+}
+
+module.exports = FoollSeq;
